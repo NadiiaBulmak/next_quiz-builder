00a1c54c93a88a3e323ac2146681ea7fbfa1bc8d
Nadiia Bulmak <nbulmak@gmail.com>
2026-07-20 13:59:37 +0300

    chore: api route

--- app/api/quizzes/route.ts
+++ app/api/quizzes/route.ts
@@ -2,6 +2,6 @@ import { prisma } from "@/lib/prisma";
 
 export async function GET() {
   const quizzes = await prisma.quiz.findMany();
-
+  console.log(quizzes)
   return Response.json(quizzes);
 }
