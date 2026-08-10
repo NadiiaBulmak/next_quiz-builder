d08cb1dbf5913e7c4c83f16e27eea28db0d2a066
Nadiia Bulmak <nbulmak@gmail.com>
2026-08-10 14:09:47 +0300

    ui: polish

--- app/(protected)/quizzes/results/page.tsx
+++ app/(protected)/quizzes/results/page.tsx
@@ -3,7 +3,7 @@ import { getCurrentUser } from "@/services/auth";
 export default async function AllQuizzes() {
   const user = await getCurrentUser();
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black  px-3 md:px-6 py-3 md:py-6 gap-4">
 
     </div>
   );
--- components/quiz/QuizList.tsx
+++ components/quiz/QuizList.tsx
@@ -11,7 +11,7 @@ export default async function QuizList({ listType }: QuizListType) {
   const typedQuizzes = quizzes as QuizListItemType[];
   console.log(typedQuizzes);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 w-full p-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 w-full">
       {quizzes.map((q) => (
         <QuizListItem key={q.id} {...q} listType={listType} />
       ))}
