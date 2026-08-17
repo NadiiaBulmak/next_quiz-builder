592f1b9fa401a0fc4bb74fa9c754ea3edf0ac88f
Nadiia Bulmak <nbulmak@gmail.com>
2026-08-17 12:40:17 +0300

    update

--- app/create-preview/page.tsx
+++ app/create-preview/page.tsx
@@ -1,6 +1,8 @@
 import { CreateQuizClient } from '@/components/quiz-create/CreateQuizClient';
 import { getCathegories } from '@/services/category.service';
 
+export const dynamic = 'force-dynamic';
+
 export default async function CreateQuizPreview() {
   const categories = await getCathegories();
 
