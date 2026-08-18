15db6d3f1c1e76af98c25a6d76bca5d049755736
Nadiia Bulmak <nbulmak@gmail.com>
2026-08-18 13:08:32 +0300

    fix: main-page iframes

--- app/create-preview/layout.tsx
+++ app/create-preview/layout.tsx
@@ -1,13 +1,13 @@
 import { TopBar } from '@/components/shared/TopBar';
 import { MobileTopBar } from '@/components/shared/MobileTopBar';
 import { MobileMenu } from '@/components/shared/MobileMenu';
+import { QuizCreateProvider } from '@/providers/QuizCreateProvider';
 
 export default async function CreatePreviewLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
   return (
     <div className="flex flex-col md:flex-row bg-stone-100">
       <main className="flex-1 flex flex-col min-h-screen transition-[padding-left] duration-300 ease-in-out lg:pl-[var(--sidebar-width)]">
@@ -23,7 +23,7 @@ export default async function CreatePreviewLayout({
           previewMode={true}
           className="hidden lg:flex"
         />
-        {children}
+        <QuizCreateProvider>{children}</QuizCreateProvider>
         <MobileMenu />
       </main>
     </div>
